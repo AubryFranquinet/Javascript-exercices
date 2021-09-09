089ece8054ab98dc5c4827eea08896c9d059aacb
Aubry Franquinet <aubry.franquinet@student.vinci.be>
2021-09-09 12:16:44 +0200

    Addition of 2.6 and 2.7

--- 2.6.js
+++ 2.6.js
@@ -1,6 +1,23 @@
-let n = window.prompt("Enter a number");
+let number = window.prompt("Enter a number between 1 and 7");
 
-
-for(let i = 0; i<=n;i++){
-    console.log(n);
+if (number == 1){
+    console.log(number + " correspond to Monday" )
+}
+else if(number == 2){
+    console.log(number + " correspond to Tuesday" )
+}
+else if(number == 3){
+    console.log(number + " correspond to Wednesday" )
+}
+else if(number == 4){
+    console.log(number + " correspond to Thursday" )
+}
+else if(number == 5){
+    console.log(number + " correspond to Friday" )
+}
+else if(number == 6){
+    console.log(number + " correspond to Saturday" )
+}
+else if(number == 7){
+    console.log(number + " correspond to Sunday" )
 }
